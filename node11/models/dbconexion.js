7adf02a8918bce8fc9abbfee302fdf74eb3f2044
marceloarmando <mcuellarr98@gmail.com>
2020-06-23 11:50:59 -0500

    commit 2

--- node11/models/dbconexion.js
+++ node11/models/dbconexion.js
@@ -1,10 +1,9 @@
 var mysql = require('mysql');
 var conn = mysql.createConnection({
-  host: 'us-cdbr-iron-east-01.cleardb.net',
-  port:3306,
-  user: 'bda87bde2321a4',
-  password: '51ad6e9a',
-  database: 'heroku_df2f4a10ff3ea9d'
+  host:"localhost",
+  user:"root",
+  password:"",
+  database:"tiendadwa"
 });
 conn.connect();
 module.exports = conn;
