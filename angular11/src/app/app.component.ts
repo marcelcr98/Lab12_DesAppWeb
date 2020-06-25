4f58beb169c67e8291f503e935db98be00eb1ba2
marceloarmando <mcuellarr98@gmail.com>
2020-06-24 23:00:37 -0500

    Commit 3

--- angular11/src/app/app.component.ts
+++ angular11/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { ProductosService } from './services/productos.service';
 import { HttpClient } from '@angular/common/http';
-import { UploadService } from './services/upload.service';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +8,7 @@ import { UploadService } from './services/upload.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  uploadedFiles: Array <File>;
+  uploadedFiles: File;
   title = 'Lab11';
   lista=null;
   prod: any = {
@@ -19,7 +18,7 @@ export class AppComponent {
     precio:null,
     cantidad:null
   }
-  constructor(private productosServicio: ProductosService, private serviceUpload: UploadService) {}
+  constructor(private productosServicio: ProductosService) {}
 
   ngOnInit() {
     this.recuperarTodos();
@@ -77,28 +76,18 @@ export class AppComponent {
     cantidad:null
     };
   }
-  
- fileChange(element) {
-    console.log('fileChangexx');
-    this.uploadedFiles = element.target.files;
-  }
-  upload() {
-    console.log('uploadxxx');
-    let formData = new FormData();
-    console.log('uploadedFiles:');
-    console.log(this.uploadedFiles);
-    for (var i = 0; i < this.uploadedFiles.length; i++) {
-      console.log('Inicio FOR');
-      // formData.append("file[]",this.uploadedFiles[0]);
-      formData.append("file",
-      this.uploadedFiles[i],
-      this.uploadedFiles[i].name);
 
+    //Agregado
+    fileChange(element) {
+      this.uploadedFiles = element.target.files;
+      console.log(this.uploadedFiles);
     }
-    console.log('formData:');
-    console.log(formData);
-    this.serviceUpload.uploadFile(formData).subscribe((res)=> {
-      console.log('response received is ', res);
-    });
-  }
+    upload() {
+      let producto = new FormData();
+      this.productosServicio.nuevo(producto).subscribe((res)=> {
+        console.log('response received is ', res);
+      });
+    }
+
+
 }
